430856639dc1a20be94814d0e21f13f52c980a6d
Faisal-Edunet <mfaisal@edunetfoundation.org>
2025-10-31 14:10:25 +0530

    added javascript files

---
+++ Html/factorial.js
@@ -0,0 +1,38 @@
+// function factorial(num)
+// {
+//     let fact = 1;
+//     if(num == 0 || num ==1)
+//     {
+//         fact = 1;
+//     }
+//     else{
+//         for(i = num ; i >=2 ; i--)
+//         {
+//             fact = fact * i
+//         }
+//     }
+//     console.log(fact)
+// }
+
+// function display(fun)
+// {
+//     for(let i  = 1 ; i<= 10 ; i++)
+//     {
+//         fun(i);
+//     }
+// }
+
+
+// display(factorial)
+
+
+// arr = [1,2,3,4,5,6,7,8,9,10]
+
+// arr2 = arr.map((num)=> num * 2);
+
+// console.log(arr2)
+
+
+console.log(a);
+var a = 10;
+console.log(a)
